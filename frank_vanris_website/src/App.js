b2a181821581a13233028dcf603c2655f3b46718
Frank van Ris <frankvanris@intouch365.nl>
2025-12-09 15:42:57 +0100

    Created a simple change to ensure I know what I am doing in React

--- frank_vanris_website/src/App.js
+++ frank_vanris_website/src/App.js
@@ -4,22 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to my Website!</h1>
+      <p>Hi, I'm Frank Christopher van Ris</p>
+      <p>This is my personal website</p>
     </div>
   );
 }
-
 export default App;
