0f5abb87d5be6fe39cc0577c2377be253b914325
FrankCVanris <frankcvanris@gmail.com>
2025-12-29 11:19:52 +0100

    Updates to the Update page

--- docs/src/pages/Updates.jsx
+++ docs/src/pages/Updates.jsx
@@ -14,6 +14,15 @@ const Updates = () => {
             <p> Today I am still working on my website I migrated my current setup to React Vite, since it is a lot more modern. Compared to regular
                 React App. So I made the shift so that I can develop in a faster and more clean environment.
             </p>
+            <p></p>
+            <h2>Update 3 -- 29/12/2025</h2>
+            <p> Today I finally was able to publish my website successfully within Github Pages. Now everything is working as intended. That was the plan,
+                Overall I've been quite lazy, but I plan to make decent progress on my website bit by bit. I can see myself progressing a bunch within certain
+                areas of my website. Especially future projects, personal blog updates, and much more. I'm also going to see if this static website can have 
+                different domain names to since it only correlates to my github name and io format of github. I'm already intrigued by what I can offer through
+                github pages. Anyways small progress today finally, tomorrow I'll see if I can stylize the front page. (AGAIN) I am not really a front end person
+                so don't expect any crazy fancy stuff. I will most likely ask gpt to make things stylish while I focus more on the mechanics of this website.S
+            </p>
         </div>
     );
 }
