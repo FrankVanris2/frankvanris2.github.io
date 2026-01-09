56182c1a9d784cbfd4b0211134134163d71554b2
FrankCVanris <frankcvanris@gmail.com>
2026-01-09 22:13:51 +0100

    added another project in the projects page

--- docs/src/pages/Projects.jsx
+++ docs/src/pages/Projects.jsx
@@ -4,6 +4,14 @@ import ProjectCard from '../components/ProjectCard';
 
 const Projects = () => {
 
+    //Website Project Specific Elements
+    const websiteDescription = [
+        "My latest project as of this moment, as you can probably guess by now is my website! This website is currently being developed through React Vite, a modern React Environment that relys on Typescript instead of Javascript. This project in general is my personal Portfolio, but I plan to add more slowly bit by bit to make it more lively to the audience. My intentions initially are to finish the general mvp's, and then to slowly expand the website.",
+        "This new project of mine is basic in nature. I am not much of a frontend developer, and doing this project is indeed giving me some frontend experience. However my intentions for the future are clear. I plan to put most of my efforts into developing future projects that are embedded and robotics related. I also wish to expand my knowledge into future technologies and use cases. As of right now within this website I have established a total of 6 pages: Homepage, About Me page, Experience page, Projects page, Updates page, and Contacts page.",
+        "Within the website I plan on also creating personal articles on future projects and future research that I would like to present to people. I might add some backend related components but for now this will be just a general portfolio that people can look at. I do also plan on adding more projects along the way to let users know what future endeavors I have been up to. I can't wait to share more on the upcoming projects after this one. In all, this is a basic web portfolio about me with basic future ideas that I plan on doing."
+    ];
+    const websiteTechStack = ["React", "Typescript", "JavaScript", "CSS", "HTML", "JSON"]
+
     // FollowBot Specific elements
     const followBotDescription = [
         "My team and I developed FollowBot, an autonomous robot designed to follow a person while avoiding obstacles. As our college capstone, we built both the hardware and software—3D‑printing custom parts in Fusion360 and FreeCAD, and prototyping early functionality with PlatformIO on an Arduino Uno Rev4 WiFi. We also created a React website with a Flask API to interact with the robot, with plans for a future mobile app.",
@@ -25,6 +33,17 @@ const Projects = () => {
             <p> The Following projects below showcase my skills and experience in various programming languages, frameworks, and tools. Each project highlights my ability to apply my knowledge to real-world scenarios and solve complex problems. Feel free to explore the projects to see the range of my capabilities and the quality of my work.
             </p>
 
+            <ProjectCard
+                title="My Personal Website"
+                tags={websiteTechStack}
+                description={websiteDescription}
+                imageSrc="/WebsiteProject.png"
+                alignment="right"
+                links={[
+                    {text: "Frank Vanris Website Repo", url: "https://github.com/FrankVanris2/frankvanris2.github.io"}
+                ]}
+            />
+            
             <ProjectCard
                 title="FollowBot"
                 tags={followBotTechStack}
